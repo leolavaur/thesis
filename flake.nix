3d32ab0caa3f23e26c54064903d72f83638d73ac
phdcybersec <82591009+phdcybersec@users.noreply.github.com>
2024-01-16 14:57:46 +0100

    :sparkles: Initial commit.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "PhD thesis manuscript.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    latex-toolbox = {
+      url = "github:phdcybersec/latex-toolbox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, latex-toolbox } @ inputs:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { 
+          inherit system;
+        };
+        latex = inputs.latex-toolbox.lib.${system}.latex;
+      in rec {
+        
+        packages = rec {
+          document = latex.mkDocument { src = ./src; main = "document.tex"; };
+          default = document;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = packages.document.buildInputs;
+          };
+        };
+
+      }
+    );
+
+}
